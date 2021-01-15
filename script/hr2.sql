5c607861c992926439b886c264b148381158af19
a <a@a.com>
2021-01-15 16:53:36 +0900

    a

---
+++ script/hr2.sql
@@ -0,0 +1,11 @@
+drop user hr2 cascade;
+
+create user hr2 identified by hr2 default tablespace users;
+grant connect, resource to hr2;
+
+create table hr2.employees (
+emp_id number(3) constraint emp_empid_pk primary key,
+emp_name varchar2(10) constraint emp_epmname_nn not null,
+hire_date date constraint emp_hiredate_nn not null);
+
+create sequence hr2.emp_empid_seq;
